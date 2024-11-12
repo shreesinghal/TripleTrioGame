e6d68ba2bb6220bf46aff662946a90e4b14c1076
jessicajain111 <jessicajain111@gmail.com>
2024-11-12 13:58:31 -0500

    Adding the strategies

--- src/cs3500/tripletrios/strategies/CornerStrategy.java
+++ src/cs3500/tripletrios/strategies/CornerStrategy.java
@@ -1,9 +1,24 @@
 package cs3500.tripletrios.strategies;
 
 import cs3500.tripletrios.model.Card;
+import cs3500.tripletrios.model.Cell;
+import cs3500.tripletrios.model.Posn;
+import cs3500.tripletrios.model.ReadOnlyTripleTrioModel;
 
 public class CornerStrategy implements TripleTrioStrategy {
 
+  private ReadOnlyTripleTrioModel model;
+
+  private Card chosenCard;
+
+  private Posn chosenLocation;
+
+
+  /**
+   * A method that allows for strategically choosing which card to play.
+   *
+   * @return the card chosen to play
+   */
   @Override
   public Card chooseCardFromHand() {
     return null;
@@ -11,6 +26,27 @@ public class CornerStrategy implements TripleTrioStrategy {
 
   @Override
   public void placeCardToGrid() {
+    Cell cell1 = model.getCurrentGrid().get(0).get(0);
+    Cell cell2 = model.getCurrentGrid().get(model.getGridWidth()).get(0);
+    Cell cell3 = model.getCurrentGrid().get(0).get(model.getGridHeight());
+    Cell cell4 = model.getCurrentGrid().get(model.getGridWidth()).get(model.getGridHeight());
 
+    if (!cell1.isEmpty() && cell1.getCellType() != Cell.CellType.HOLE) {
+
+    }
+    if (!cell2.isEmpty() && cell2.getCellType() != Cell.CellType.HOLE) {
+
+    }
+    if (!cell3.isEmpty() && cell3.getCellType() != Cell.CellType.HOLE) {
+
+    }
+    if (!cell4.isEmpty() && cell4.getCellType() != Cell.CellType.HOLE) {
+
+    }
   }
+
+
+
+
+
 }
