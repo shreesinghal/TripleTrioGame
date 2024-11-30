86b8e2fe20d2de62f364599067c24cc8da4bfdcb
jessicajain111 <jessicajain111@gmail.com>
2024-11-30 12:34:16 -0500

    added modelfeatures and viewfeatures adapter classes

---
+++ src/cs3500/threetrios/providers/controller/ViewFeaturesAdapterOursToTheirs.java
@@ -0,0 +1,27 @@
+package cs3500.threetrios.providers.controller;
+
+import cs3500.threetrios.providers.model.PlayerType;
+
+public class ViewFeaturesAdapterOursToTheirs implements ViewFeatures{
+  /**
+   * Selects the given card index as the card to play.
+   *
+   * @param cardIdx    the card index
+   * @param playerType
+   */
+  @Override
+  public void selectedCard(int cardIdx, PlayerType playerType) {
+
+  }
+
+  /**
+   * Plays the current selected card to the coordinate.
+   *
+   * @param row    the row (0-based)
+   * @param column the column (0-based)
+   */
+  @Override
+  public void playAtBoardPoint(int row, int column) {
+
+  }
+}
