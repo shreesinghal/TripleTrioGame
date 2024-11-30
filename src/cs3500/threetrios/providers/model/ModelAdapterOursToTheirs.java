c4765f7228e98be1053430fb5d3d8c7bb50f1e3f
jessicajain111 <jessicajain111@gmail.com>
2024-11-30 12:27:58 -0500

    implemented and finished(??) model adapter class. Need to look over start game and addListener

--- src/cs3500/threetrios/providers/model/ModelAdapterOursToTheirs.java
+++ src/cs3500/threetrios/providers/model/ModelAdapterOursToTheirs.java
@@ -1,14 +1,13 @@
 package cs3500.threetrios.providers.model;
 
 import cs3500.threetrios.providers.controller.ModelFeatures;
+import cs3500.threetrios.providers.model.card.CardAdapterOursToTheirs;
 import cs3500.tripletrios.model.CardColor;
-import cs3500.tripletrios.model.Player;
 import cs3500.tripletrios.model.TripleTrioModel;
 import cs3500.tripletrios.model.WinningState;
-import cs3500.threetrios.providers.model.PlayerType;
 import cs3500.threetrios.providers.model.card.Card;
 import cs3500.threetrios.providers.model.cell.Cell;
-import model.rule.BattleRule;
+import cs3500.threetrios.providers.model.rule.BattleRule;
 import cs3500.tripletrios.model.Cell.CellType;
 
 import java.util.ArrayList;
@@ -26,6 +25,7 @@ public class ModelAdapterOursToTheirs implements TripleTriad {
   }
 
   /**
+   *
    * @param listener
    */
   @Override
@@ -33,22 +33,35 @@ public class ModelAdapterOursToTheirs implements TripleTriad {
 
   }
 
-  /**
-   * Starts the game with the given list of types of players and the
-   * dimensions of the board. Starts with Player A as first player.
-   *
-   * @param grid     starting 2D array of cells configuration
-   * @param allCards all the cards to start the game with
-   * @param rule     the battling rule of the game
-   * @throws IllegalStateException    if the game has already started or the game is over
-   * @throws IllegalArgumentException if the board or deck configurations are null
-   * @throws IllegalArgumentException if the number of cards is not at least one greater than
-   *                                  the number of card cells in the board
-   * @throws IllegalArgumentException if the names of cards are not unique
-   */
   @Override
   public void startGame(Cell[][] grid, List<Card> allCards, BattleRule rule) {
-    ourModel.startGame();
+    //we need a way to set PlayerA as current player and PlayerB as opposing player
+    //ourModel.startGame(createGrid(), createDeck()   );
+    //we cannot implement this because our model implementation doesn't have a
+    //battle rule interface
+  }
+
+
+  private List<Card> createDeck() {
+    List<Card> allCards = new ArrayList<>();
+    for (cs3500.tripletrios.model.Card card : ourModel.getDeck()) {
+      allCards.add(new CardAdapterOursToTheirs(card));
+    }
+
+    return allCards;
+  }
+
+  private Cell[][] createGrid() {
+    ArrayList<ArrayList<cs3500.tripletrios.model.Cell>> ourGrid = ourModel.getCurrentGrid();
+    Cell[][] theirGrid = new Cell[ourGrid.size()][ourGrid.get(0).size()];
+
+    for (int row = 0; row < ourGrid.size(); row++) {
+      for (int col = 0; col < ourGrid.get(0).size(); col++) {
+        cs3500.tripletrios.model.Cell originalCell = ourGrid.get(row).get(col);
+      }
+    }
+
+    return theirGrid;
   }
 
   /**
@@ -75,25 +88,24 @@ public class ModelAdapterOursToTheirs implements TripleTriad {
   /**
    * Returns the hand of the given player.
    *
-   * @param playerType
+   * @param playerType the player whose hand we want
    * @return the hand of the player of the playerType
    * @throws IllegalArgumentException if playerType is null
    * @throws IllegalStateException    if the game has not started
    */
   @Override
   public List<Card> fetchHand(PlayerType playerType) {
-    return playerType.equals(PlayerType.PLAYER_A)
-            ? arrayListToList(ourModel.getPlayer().getHand())
+    return playerType.equals(PlayerType.PLAYER_A) ?
+            arrayListToList(ourModel.getPlayer().getHand())
             : arrayListToList(ourModel.getOppPlayer().getHand());
   }
 
-  private List<model.card.Card> arrayListToList(ArrayList<cs3500.tripletrios.model.Card> ourHand) {
-    List<model.card.Card> cards = new ArrayList<>();
+  private List<Card> arrayListToList(ArrayList<cs3500.tripletrios.model.Card> ourHand) {
+    List<Card> playerHand = new ArrayList<>();
     for (cs3500.tripletrios.model.Card ourCard : ourHand) {
-      Card theirCard = CardAdapterOursToTheirs(ourCard);
-      cards.add(theirCard);
+      playerHand.add(new CardAdapterOursToTheirs(ourCard));
     }
-    return cards;
+    return playerHand;
   }
 
   /**
@@ -137,6 +149,7 @@ public class ModelAdapterOursToTheirs implements TripleTriad {
   @Override
   public int fetchPlayerScore(PlayerType player) {
     //cannot adapt this because our models getPlayerScore() method is private
+    return 0;
   }
 
   /**
@@ -171,6 +184,7 @@ public class ModelAdapterOursToTheirs implements TripleTriad {
   public int numCardsPlayerCanFlip(int row, int column, Card card, PlayerType player) {
     //we cannot implement this because our executeBattlePhase() does this using private
     //helper methods
+    return 0;
   }
 
   /**
@@ -249,6 +263,6 @@ public class ModelAdapterOursToTheirs implements TripleTriad {
    */
   @Override
   public Card cardAtCoordinate(int row, int column) {
-    return null;
+    return new CardAdapterOursToTheirs(ourModel.getCurrentGrid().get(row).get(column).getCard());
   }
 }
